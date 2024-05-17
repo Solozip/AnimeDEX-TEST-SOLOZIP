6604125d85156fef418cc288a9212dd3b3c1de5b
Solozip <136900323+Solozip@users.noreply.github.com>
2024-05-17 20:38:36 +0530

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,11 +1,11 @@
 // Api urls
 
-const ProxyApi = "https://proxy.techzbots1.workers.dev/?u=";
+const ProxyApi = "https://proxy.ar5102935.workers.dev/?u=https://animaxforfree.vercel.app";
 const searchapi = "/search/";
 
 // Api Server Manager
 
-const AvailableServers = ["https://api100.anime-dex.workers.dev"];
+const AvailableServers = ["https://api3.ar5102935.workers.dev"];
 
 function getApiServer() {
     return AvailableServers[Math.floor(Math.random() * AvailableServers.length)];
