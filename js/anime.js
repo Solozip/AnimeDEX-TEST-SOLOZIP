f2c6493872d8321be84c8ae6dab6cd27464f8c3b
Solozip <136900323+Solozip@users.noreply.github.com>
2024-05-17 20:34:38 +0530

    Update anime.js

--- js/anime.js
+++ js/anime.js
@@ -1,12 +1,12 @@
 // Api urls
 
-const ProxyApi = "https://proxy.techzbots1.workers.dev/?u=";
+const ProxyApi = "https://proxy.ar5102935.workers.dev/?u=http://animax.rf.gd";
 const animeapi = "/anime/";
 const recommendationsapi = "/recommendations/";
 
 // Api Server Manager
 
-const AvailableServers = ["https://api100.anime-dex.workers.dev"];
+const AvailableServers = ["https://api3.ar5102935.workers.dev"];
 
 function getApiServer() {
     return AvailableServers[Math.floor(Math.random() * AvailableServers.length)];
